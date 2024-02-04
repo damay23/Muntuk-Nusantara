3bb539e3a925902721ba5338853e9881faffba99
Damayanti Dwi Astuti <damayanti.d.a@mail.ugm.ac.id>
2024-02-04 21:19:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// header scroll
+const header = document.querySelector('header');
+const menu = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
+
+// Swiper
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
